aae0a8242534c09646b51043ded60881b88ab454
Jon Yu <jon.yu.1105@gmail.com>
2018-12-08 15:04:33 -0500

    Using CrossPlatformInputManager

---
+++ TwinStick/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Horizontal: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("Vertical: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
