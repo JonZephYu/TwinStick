b491b8173bf8d5e72d83e6c58c2070db62a6d041
Jon Yu <jon.yu.1105@gmail.com>
2018-12-18 15:09:44 -0500

    Created pause system

--- TwinStick/Assets/Game/GameManager.cs
+++ TwinStick/Assets/Game/GameManager.cs
@@ -6,30 +6,66 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
     private bool recording = true;
+    private bool paused = false;
+    private float defaultDeltaTime;
+    //public static enum Status {RECORD, PLAYBACK, REWIND, PAUSE};
 
     private void Start() {
         PlayerPrefsManager.UnlockLevel(1);
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(2));
-    }
+        defaultDeltaTime = Time.fixedDeltaTime;
+        Debug.Log(defaultDeltaTime);
+
+}
 
     // Update is called once per frame
     void Update() {
-        if (CrossPlatformInputManager.GetButton("Fire1")) {
-            Debug.Log("Playback mode");
-            recording = false;
+        if (!paused) {
+            if (CrossPlatformInputManager.GetButton("Fire1")) {
+                Debug.Log("Playback mode");
+                recording = false;
+            }
+            else if (CrossPlatformInputManager.GetButton("Fire2")) {
+                Debug.Log("Rewind mode");
+                recording = false;
+            }
+            else {
+                Debug.Log("Record mode");
+                recording = true;
+            }
         }
-        else if (CrossPlatformInputManager.GetButton("Fire2")) {
-            Debug.Log("Rewind mode");
-            recording = false;
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (!paused) {
+                PauseGame();
+            }
+            else {
+                ResumeGame();
+            }
         }
-        else {
-            Debug.Log("Record mode");
-            recording = true;
-        }
+
     }
 
     public bool isRecording() {
         return recording;
     }
+
+    private void PauseGame() {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+        paused = true;
+    }
+
+    private void ResumeGame() {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = defaultDeltaTime;
+        paused = false;
+    }
+
+    //This message is sent by the application to pause, i.e. mobile switch windows
+    private void OnApplicationPause(bool pause) {
+        paused = pause;
+    }
+
 }
